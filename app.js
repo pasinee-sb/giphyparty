8bac023c2994bfe8dcdc446deff2663d9560ab72
Pasinee <pasinee.sb@gmail.com>
2023-01-24 22:41:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+$("form").
+on("submit", function(evt)
+{evt.preventDefault();
+searchGiphy($("input").val());
+
+
+});
+
+async function searchGiphy(word){
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=cV0tlNG65o5QnWVuGhhVMafKo3BpeeHT&q=${word}`);
+    const myGiph = res.data.data[Math.floor(Math.random()*(res.data.data.length))].images.original.url;
+
+    // const giphy = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = myGiph;
+    giphy.append(img);
+    $('#giphies').append(img);
+    console.log(img.src);
+
+
+}
+
+$("#remove").on('click', function(){
+    $('#giphies').empty();
+})
